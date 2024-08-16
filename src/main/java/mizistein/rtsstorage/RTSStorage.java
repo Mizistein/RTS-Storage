8fc0a9e0ca2e6c52114e63f603708625e1bf2d9b
Mizistein <mizisteim@gmail.com>
2024-08-16 20:41:10 +0200

    Initial commit

---
+++ src/main/java/mizistein/rtsstorage/RTSStorage.java
@@ -0,0 +1,17 @@
+package mizistein.rtsstorage;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RTSStorage implements ModInitializer {
+	public static final String MOD_ID = "rtsstorage";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/mizistein/rtsstorage/RTSStorageClient.java
@@ -0,0 +1,12 @@
+package mizistein.rtsstorage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RTSStorageClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
